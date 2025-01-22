5e008d48b5722c98b2638103ce1459011c38819b
UjjwalKraken <ujjwal.solanki@epita.fr>
2025-01-22 23:02:49 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/' // Replace with your repository name
+  base: '/portfolio-website/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
